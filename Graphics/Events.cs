d323ea37a12b35d0ad52693d2678600c93b4b2f0
wbarret1 <barrett.williamm@epa.gov>
2017-03-10 08:08:28 -0500

    Separated Graphics into thier own library.

--- Graphics/Events.cs
+++ Graphics/Events.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Graphics
+namespace SustainableChemistry
 {
 
     public class SelectionChangedEventArgs : System.EventArgs
